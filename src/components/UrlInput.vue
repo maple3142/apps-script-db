d4abc0295bf178853a190505d85121a5730e30a1
maple3142 <kirby741852963@gmail.com>
2018-03-25 00:26:05 +0800

    fix copy

--- src/components/UrlInput.vue
+++ src/components/UrlInput.vue
@@ -30,7 +30,7 @@ export default {
 			const { protocol, hostname, port, pathname } = location
 			let ar = [protocol, '//', hostname]
 			if (port !== 80) ar = ar.concat([':', port])
-			ar = ar.concat([pathname], '?url=', url)
+			ar = ar.concat([pathname], '?url=', encodeURIComponent(url))
 			const str = ar.join('')
 			const r = copy(str)
 			if (!r) prompt(this.$t('copy.failed'), str)
